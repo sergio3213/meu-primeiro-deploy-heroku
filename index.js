de1849492c64314c4387c5d79fbcc821c8ff88e6
Sergio <sergio.n.m.jr@gmail.com>
2022-06-05 09:39:37 -0300

    t

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(`<h1>executaaando na porta:${port}</h1>`));
