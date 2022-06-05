704cfeb30e662a9a793c94ad4b5f52b95982e034
Sergio <sergio.n.m.jr@gmail.com>
2022-06-05 09:20:06 -0300

    correcoes lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send(`<h1>executaando na porta:${port}</h1>`));
-app.listen(port,console.log(`Online aqi: ${port}`))
+app.get('/', (req, res) => res.send(`<h1>executaando na porta:${port}</h1>`));
+app.listen(port, console.log(`Online aqi: ${port}`));
